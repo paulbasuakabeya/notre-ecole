5cefb5b9638f2419b0f4157ba72f2081bacd0593
paulbasuakabeya <135709152+paulbasuakabeya@users.noreply.github.com>
2025-03-03 14:19:10 +0000

    Update login.jsx

--- src/components/devoir/login.jsx
+++ src/components/devoir/login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-            const response = await axios.post("http://localhost:3000/api/auth/login", { email, password });
+            const response = await axios.post("https://backende-murex.vercel.app/api/auth/login", { email, password });
       localStorage.setItem("token", response.data.token);
       localStorage.setItem("role", response.data.role);
       navigate("/navbartwo");
