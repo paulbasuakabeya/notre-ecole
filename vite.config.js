3bf4a78ff5cae64860603f3dd3953839778e93ab
paulbasuakabeya <paulbasuakabeya@gmail.com>
2025-03-03 12:55:40 +0000

    Correction des conflits et rebase terminé

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [
+        '@react-google-maps/api'
+      ]
+    }
+  }
 })
