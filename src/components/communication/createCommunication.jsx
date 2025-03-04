0e3662064ef4cffdc7eeb1501751d844486f2433
paulbasuakabeya <135709152+paulbasuakabeya@users.noreply.github.com>
2025-03-04 00:03:29 +0000

    Update createCommunication.jsx

--- src/components/communication/createCommunication.jsx
+++ src/components/communication/createCommunication.jsx
@@ -14,7 +14,7 @@ const CreateCommunication = () => {
     const newCommunication = { titre, message, date };
 
     try {
-      await axios.post(`http://localhost:3000/api/communications`, newCommunication, {
+      await axios.post(`https://backende-ten.vercel.app//api/communications`, newCommunication, {
         headers: { Authorization: `Bearer ${token}` },
       });
       navigate("/communications");
