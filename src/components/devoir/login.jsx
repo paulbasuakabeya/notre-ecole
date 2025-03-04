514824f955d866232f39afddcd527cb73d4e9bb1
paulbasuakabeya <135709152+paulbasuakabeya@users.noreply.github.com>
2025-03-04 07:58:47 +0000

    Update login.jsx

--- src/components/devoir/login.jsx
+++ src/components/devoir/login.jsx
@@ -22,17 +22,39 @@ const Login = () => {
   
 
   return (
-    <div
-          className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
-          style={{ backgroundImage: `url(${backgroundImage})` }}
-        >
-      {/* <h2 className=" bg-blue-300 text-2xl font-bold mb-4">Connexion</h2> */}
-      <form onSubmit={handleSubmit} className="bg-blue-300 p-6 rounded shadow-md w-80">
-        <input type="email" placeholder="Email" className="border p-2 w-full mb-2" onChange={(e) => setEmail(e.target.value)} />
-        <input type="password" placeholder="Mot de passe" className="border p-2 w-full mb-4" onChange={(e) => setPassword(e.target.value)} />
-        <button type="submit" className="px-4 py-2 bg-blue-300 text-white rounded w-full">Se connecter</button>
-      </form>
-    </div>
+     <div
+  className="relative w-full h-1/2 md:h-screen min-h-[50vh] flex flex-col items-center justify-center bg-cover bg-center 
+             overflow-hidden max-w-full"
+  style={{
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    aspectRatio: "16/9",
+  }}
+>
+  {/* Overlay pour améliorer la lisibilité */}
+  <div className="absolute inset-0 bg-black/25"></div>
+
+  {/* Formulaire */}
+  <form 
+    onSubmit={handleSubmit}  className="relative z-10 bg-blue-300 p-6 rounded-xl shadow-md w-full max-w-xs md:max-w-md">
+    <input type="email" placeholder="Email" className="border border-gray-300 p-2 w-full mb-2 rounded-md" onChange={(e) => setEmail(e.target.value)} />
+    <input type="password" placeholder="Mot de passe" className="border border-gray-300 p-2 w-full mb-4 rounded-md" onChange={(e) => setPassword(e.target.value)} />
+    <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md w-full hover:bg-blue-600 transition" >Se connecter</button>
+  </form>
+</div>
+    // <div
+    //       className="min-h-screen flex flex-col items-center justify-center bg-cover bg-center"
+    //       style={{ backgroundImage: `url(${backgroundImage})` }}
+    //     >
+    //   {/* <h2 className=" bg-blue-300 text-2xl font-bold mb-4">Connexion</h2> */}
+    //   <form onSubmit={handleSubmit} className="bg-blue-300 p-6 rounded shadow-md w-80">
+    //     <input type="email" placeholder="Email" className="border p-2 w-full mb-2" onChange={(e) => setEmail(e.target.value)} />
+    //     <input type="password" placeholder="Mot de passe" className="border p-2 w-full mb-4" onChange={(e) => setPassword(e.target.value)} />
+    //     <button type="submit" className="px-4 py-2 bg-blue-300 text-white rounded w-full">Se connecter</button>
+    //   </form>
+    // </div>
     
   );
 };
